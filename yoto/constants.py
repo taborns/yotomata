7ee0f8b4574c5b28b627ac8e730ac0dca28abe28
Tabor Nekatibeb Shiferaw <shiferawtabor@gmail.com>
2018-08-29 11:22:28 +0300

    constants.py fiksed

--- yoto/constants.py
+++ yoto/constants.py
@@ -1,4 +1,4 @@
 import os
-HOST = os.environ("MYSITE_HOST", "http://li965-123.members.linode.com")
-DOMAIN = os.environ("MYSITE_DOMAIN", "http://li965-123.members.linode.com")
-NUM_THREADS = os.environ("MYSITE_NUMTHREADS", 6);
+HOST = os.environ.get("MYSITE_HOST", "http://li965-123.members.linode.com")
+DOMAIN = os.environ.get("MYSITE_DOMAIN", "http://li965-123.members.linode.com")
+NUM_THREADS = os.environ.get("MYSITE_NUMTHREADS", 6);
